54ff59afd676d62fd2639ffaee4331fe15b7f12a
Albert <123456@qq.com>
2020-07-06 13:35:15 +0800

    private和public

--- 21天学通C++/Execise_cpp/9.1_class.cpp
+++ 21天学通C++/Execise_cpp/9.1_class.cpp
@@ -4,10 +4,21 @@ using namespace std;
 
 class Human
 {
-public:
+private:    //将属性设置为私有的，不能在外部直接访问它
 	int age;
 	string name;
-	
+
+public:    //将方法设置为共有，供外部调用
+	void SetAge(int inputAge)
+	{
+		age = inputAge;
+	}
+
+	void SetName(string inputName)
+	{
+		name = inputName;
+	}
+
 	void Introduction()
 	{
 		cout << "Myname name is " << name;
@@ -18,12 +29,12 @@ public:
 int main(void)
 {
 	Human firstMan;
-	firstMan.age = 25;
-	firstMan.name = "Liuxuewen";
+	firstMan.SetAge(25);
+	firstMan.SetName("Liuxuewen");
 
 	Human firstWoman;
-	firstWoman.age = 24;
-	firstWoman.name = "Goaman";
+	firstWoman.SetAge(24);
+	firstWoman.SetName("Gaoman");
 
 	firstMan.Introduction();
 	firstWoman.Introduction();
